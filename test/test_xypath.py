ea44df1acca0cf16e1d82c0ecea29f33ebd35b49
Dragon Dave <dragon@scraperwiki.com>
2013-05-23 16:10:25 +0100

    tests

---
+++ test/test_xypath.py
@@ -0,0 +1,41 @@
+import sys
+from nose.tools import *
+sys.path.append('code')
+import xypath
+import messytables
+import re
+import xy
+
+class Test_XYPath(object):
+    @classmethod
+    def setup_class(klass):
+        messy = messytables.excel.XLSTableSet(open("fixtures/wpp.xls", "rb"))
+        klass.table = xypath.Table.from_messy(messy.tables[0])
+
+    def setUp(self):
+        pass
+
+    def test_has_table(self):
+        assert type(self.table) == xypath.Table
+        assert isinstance(self.table, xypath.Bag)
+
+    def test_basic_vert(self):
+        r = repr(self.table.match(lambda b: b.x == 2))
+        assert "WORLD" in r
+        assert "Country code" not in r
+
+    def test_basic_horz(self):
+        r = repr(self.table.match(lambda b: b.y == 16))
+        assert "WORLD" not in r
+        assert "Country code" in r
+
+    def test_text_search(self):
+        def textsearch(s):
+            return lambda b: re.search(s, unicode(b))
+        self.table.match(textsearch("Country.code")).assertone()
+
+    def test_intersection(self):
+        a = self.table.match(xy.textsearch("WORLD")).getit()
+        b = self.table.match(xy.textsearch("1990-1995")).getit()
+        c = a.intersection(b).getit()
+        assert c.value==1.523
