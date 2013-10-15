d1fa5927fdcd75f3cb91548aa72cbf3dbb7e0454
Zarino Zappia <mail@zarino.co.uk>
2013-10-15 17:31:24 +0100

    clearer variable names etc.

--- xyzzy_example.py
+++ xyzzy_example.py
@@ -12,8 +12,8 @@ dimensions_horizontal = [0,1]
 dimensions_vertical = [1]
 
 
-def header(table, dimension, **kwargs):
-    index, is_row = dimension
+def get_value_bags_for_dimension(table, header_index_and_direction, **kwargs):
+    index, is_row = header_index_and_direction
     if is_row:
         function, direction = (lambda cell: cell.y == index, xypath.DOWN)
     else:
@@ -24,23 +24,13 @@ def header(table, dimension, **kwargs):
 
 
 def xtract_by_numbers(table, rows, columns):
-    lookup = [(row_index, True) for row_index in rows] +\
-             [(column_index, False) for column_index in columns]
-    descriptions = [header(table, x) for x in lookup]
-    description_dict = OrderedDict(('dim_%s' % (i+1), x) for i, x in enumerate(descriptions))
-    return xypath.xyzzy.xyzzy(table, description_dict)
+    rows_and_columns = [(row_index, True) for row_index in rows] +\
+                       [(column_index, False) for column_index in columns]
+    bags_by_dimension = [get_value_bags_for_dimension(table, x) for x in rows_and_columns]
+    def name_dim(index, dimension):
+        return ('dimension_%s' % (index + 1), dimension)
+    bags_for_xyzzy = OrderedDict(name_dim(i, x) for i, x in enumerate(bags_by_dimension))
+    return xypath.xyzzy.xyzzy(table, bags_for_xyzzy, 'value')
 
 for thing in xtract_by_numbers(xy, dimensions_horizontal, dimensions_vertical):
     print thing
-
-
-
-# lookup = {'caps': (lambda cell: cell.y == 0, xypath.DOWN),
-#           'lower': (lambda cell: cell.y == 1, xypath.DOWN),
-#           'late': (lambda cell: cell.x == 1, xypath.RIGHT)}
-#
-# description = {x: xypath.xyzzy.headers(xy, lookup[x][0], lookup[x][1]) for x in lookup}
-#
-#
-# for x in xypath.xyzzy.xyzzy(xy, description):
-#     print x
