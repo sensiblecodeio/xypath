74eb61bc9780bc2cd90fe86e724fcbc7de8ca174
Dragon Dave <dragon@scraperwiki.com>
2014-01-02 15:59:37 +0000

    xyzzy: 300% speed increase

--- xypath/xyzzy.py
+++ xypath/xyzzy.py
@@ -2,8 +2,12 @@
 
 from collections import OrderedDict
 from itertools import groupby
-import xypath
 from xypath import Bag
+
+
+class LookupFailureError(Exception):
+    pass
+
 
 def group(self, keyfunc=None):  # XYZZY
     """get a dictionary containing lists of singleton bags with the same
@@ -18,6 +22,7 @@ def group(self, keyfunc=None):  # XYZZY
         groups[k] = newbag
     return groups
 
+
 def headerheader(self, dir1, dir2, **kwargs):  # XYZZY
     """Given a header (e.g. "COUNTRY") get all things in one direction
        from it (e.g. down: "FRANCE", "GERMANY"), then use those to get
@@ -25,7 +30,9 @@ def headerheader(self, dir1, dir2, **kwargs):  # XYZZY
     header = group(self.fill(dir1), **kwargs)
     return {k: header[k].fill(dir2) for k in header}
 
+
 def are_distinct(fields):
+    """True if the fields passed to xyzzy don't contain repeating cells."""
     allbags = set()
     bagcount = 0
     for field in fields:
@@ -34,32 +41,25 @@ def are_distinct(fields):
             bagcount = bagcount + len(bag)
     return len(allbags) == bagcount
 
+
 def xyzzy(self, fields, valuename='_value'):  # XYZZY
     assert are_distinct(fields.values())
     fieldkeys = fields.keys()
     assert valuename not in fieldkeys
-    # TODO: test at this stage that fieldvalue bags don't overlap:
-    # i.e. len(union of all bags) = sum(length of each bag) for each field.
-    # instead of checking all combinations have no more than one match
-    # DONE, use are_distinct. Still need to remove existing expensive code.
+
+    def fieldlookup(cell):
+        for fieldvalue in fields[field]:  # AFG, GBR; 1998, 1999
+            if cell in fields[field][fieldvalue]:  # i.e. in bag
+                return fieldvalue
+        raise LookupFailureError
 
     for cell in self:
         path = OrderedDict()
-        for i, field in enumerate(fields):  # country, year
-            for fieldvalue in fields[field]:  # AFG, GBR; 1998, 1999
-
-                if cell in fields[field][fieldvalue]:  # i.e. in bag
-                    assert field not in path  # only one match per field
-                    path[field] = fieldvalue
-                    break  # speedup - need test at top of function, really!
-            if len(path) != i+1:
-                break  # save unnecessary work
-
-        # skip values which aren't complete in all dimensions
-        if len(path) != len(fieldkeys):
-            # if len(path) > 0:
-            #     print "found %r matches for %r: %r" % (len(path), cell, path.keys())
+        try:
+            for field in fields:  # country, year
+                path[field] = fieldlookup(cell)
+        except LookupFailureError:
             continue
-        # add dictionary of cell details to list
+
         path[valuename] = cell.value
         yield path
