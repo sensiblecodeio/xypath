d279dc6271ba6bee3da2bbe968f12894c87ef804
Dragon Dave <dragon@scraperwiki.com>
2014-01-13 14:53:28 +0000

    move labellize, all_c -> instructions

--- xypath/ravel.py
+++ xypath/ravel.py
@@ -1,5 +1,20 @@
 import xypath
-def ravel(in_bag, all_c, labels=None, value_bag=None):
+def _labellize(bag):
+    """
+    for now, just a groupby over the bag
+    need to be smarter (e.g. gap recognition)
+    * insight: is expensive and is invoked every time on a constant thing (the 2nd gen headers)
+    """
+    # for now, just groupby,
+    # in future, need to be smarter (e.g. gap recognition)
+    # might want to allow
+    import itertools
+    sort = lambda cell: cell.value
+    for label, group in itertools.groupby(sorted(bag, key=sort), sort):
+        yield label, xypath.Bag.from_list(group)
+
+
+def ravel(in_bag, instructions, labels=None, value_bag=None):
     # What can change the nature of a spreadsheet?
     """flatten? olapify? a table.
        Takes a table plus a list of lambdas which take a bag.
@@ -16,9 +31,9 @@ def ravel(in_bag, all_c, labels=None, value_bag=None):
     if labels is None:
         labels = []
 
-    all_c = list(all_c)
+    instructions = list(instructions)
 
-    if not all_c:
+    if not instructions:
         # we've bottomed out with a full set of labels and - hopefully -
         # a singleton bag which we just want the value from.
         # If there's nothing in the bag, that's fine.
@@ -26,33 +41,24 @@ def ravel(in_bag, all_c, labels=None, value_bag=None):
             yield labels, value_bag.value
         return
 
-    def labellize(bag):
-        # for now, just groupby,
-        # in future, need to be smarter (e.g. gap recognition)
-        # might want to allow
-        import itertools
-        sort = lambda cell: cell.value
-        for label, group in itertools.groupby(sorted(bag, key=sort), sort):
-            yield label, xypath.Bag.from_list(group)
-
-    current_function = all_c.pop(0)
+    current_function = instructions.pop(0)
     if current_function:
         # we're looking at a function to get the next set of headers.
         # note the use of list to ensure that we're working with copies
         # of the things we're mutating.
         label_cells = current_function(in_bag)
-        for label, bag in labellize(label_cells):
+        for label, bag in _labellize(label_cells):
             new_labels = list(labels)
             new_labels.append(label)
-            p = ravel(bag, list(all_c), new_labels, value_bag)
+            p = ravel(bag, instructions, new_labels, value_bag)
             for item in p:
                 yield item
     else:
         # there was a None, which indicates that the next item is a
         # function to get a pile of values.
-        value_function = all_c.pop(0)
+        value_function = instructions.pop(0)
         new_value_bag = value_function(in_bag).intersection(value_bag)
-        p = ravel(in_bag, list(all_c), labels, new_value_bag)
+        p = ravel(in_bag, instructions, labels, new_value_bag)
         for item in p:
             yield item
 
