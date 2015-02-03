0f18f7585552cb0326af361b394c1817d8c5d961
Dragon Dave McKee <dragon@scraperwiki.com>
2015-02-03 10:53:54 +0000

    more tests: col to number and number to col

--- test/test_excel.py
+++ test/test_excel.py
@@ -6,9 +6,20 @@ class Test_Loader(unittest.TestCase):
     def test_excel_address(self):
         assert_equal(exceltool.excel_address_coordinate("A1"), (0, 0))
         assert_equal(exceltool.excel_address_coordinate("IV1"), (255, 0))
-        # assert_equal(exceltool.excel_address_coordinate("CHRDW1001"), (2**20-1, 1000))
+        assert_equal(exceltool.excel_address_coordinate("BGQCV1001"), (2**20-1, 1000))
 
     def test_invalid_excel_address(self):
         assert_raises(exceltool.InvalidExcelReference, exceltool.excel_address_coordinate, "CAT")
 
+    def test_excel_col(self):
+        assert_equal(exceltool.excel_column_number("A"), 1)
+        assert_equal(exceltool.excel_column_number("Z"), 26)
+        assert_equal(exceltool.excel_column_number("Z")+1, exceltool.excel_column_number("AA"))
+        assert_equal(exceltool.excel_column_number("AZ")+1, exceltool.excel_column_number("BA"))
+        assert_equal(exceltool.excel_column_number("ZZ")+1, exceltool.excel_column_number("AAA"))
+        assert_equal(exceltool.excel_column_number("CAZ")+1, exceltool.excel_column_number("CBA"))
 
+    def test_col_label(self):
+        assert_equal(exceltool.excel_column_label(1), "A")
+        assert_equal(exceltool.excel_column_label(256), "IV")
+        assert_equal(exceltool.excel_column_label(2**20), "BGQCV")
