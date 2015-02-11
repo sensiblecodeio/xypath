a3cf2c475676f847a20d50ad794642b2afbea7ba
Dragon Dave McKee <dragon@scraperwiki.com>
2015-02-11 15:28:53 +0000

    Add numbers to loaded tables

--- xypath/loader.py
+++ xypath/loader.py
@@ -20,28 +20,30 @@ def get_sheets(mt_tableset, ids):
     [5, 'kitten',
     lambda table: table.get_at(0,0).value == 'dog']
     will get you the 6th tab, all tabs named 'kitten'
-    and all tabs where the top-left cell contains 'dog'.
-    Note ["*"] will get you tables named asterisk."""
+    and all tabs where the top-left cell contains 'dog'."""
 
-    if ids == '*':
-        for mt_table in mt_tableset.tables:
-            yield xypath.Table.from_messy(mt_table)
-        return
-    if isinstance(ids, int) or isinstance(ids, basestring) or callable(ids):
+    def xy():
+        xy_table = xypath.Table.from_messy(mt_table)
+        xy_table.index = table_index
+        return xy_table
+
+    if isinstance(ids, (int, basestring)) or callable(ids):
         # it's a single thing, listify it
         ids = (ids, )
-    for identifier in ids:
-        if isinstance(identifier, int):
-            mt_table = mt_tableset.tables[identifier]
-            yield xypath.Table.from_messy(mt_table)
-        elif isinstance(identifier, basestring):
-            for mt_table in mt_tableset.tables:
-                if mt_table.name == identifier:
-                    yield xypath.Table.from_messy(mt_table)
-        elif callable(identifier):
-            for mt_table in mt_tableset.tables:
-                xy_table = xypath.Table.from_messy(mt_table)
+
+    for table_index, mt_table in enumerate(mt_tableset.tables):
+        for identifier in ids:
+            if identifier == '*':
+                yield xy()
+            elif isinstance(identifier, int):
+                if identifier == table_index:
+                    yield xy()
+            elif isinstance(identifier, basestring):
+                if identifier == mt_table.name:
+                    yield xy()
+            elif callable(identifier):
+                xy_table = xy()
                 if identifier(xy_table):
                     yield xy_table
-        else:
-            raise NotImplementedError("Don't know what to do with a {!r}".format(type(identifier)))
+            else:
+                raise NotImplementedError("Don't know what to do with a {!r}".format(type(identifier)))
