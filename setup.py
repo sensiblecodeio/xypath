6c4610100ec3b038b233e8ea2aa581687237e8f3
Dragon Dave McKee <dragon@scraperwiki.com>
2015-03-03 17:34:00 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ navigating around and extracting values from tabular data.
 
 setup(
     name='xypath',
-    version='1.0.10',
+    version='1.0.11',
     description="Extract fields from tabular data with complex expressions.",
     long_description=long_desc,
     classifiers=[
@@ -28,7 +28,7 @@ setup(
     include_package_data=False,
     zip_safe=False,
     install_requires=[
-        'messytables>=0.14.3',
+        'messytables>=0.14.5',
     ],
     tests_require=[],
     entry_points=\
